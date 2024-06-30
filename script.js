cef1690d95a2f941014340f5b61411f6a7ab83b9
project 1 <radhityo.kusumo@gmail.com>
2024-06-30 17:47:21 +0700

    coba bikin responsive di device lain

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky", window.scrollY 60)
+    header.classList.toggle("sticky", window.scrollY > 60)
 });
+
+const hamMenu = docimen.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-mmenu');
+
+hamMenu.addEventListener('click', () --> { hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
